2cdbc1500d0842cd5b25ad175451aeab0fda566a
Erica Peharda <ericapeharda@gmail.com>
2015-09-18 09:47:50 -0600

    updating the colorado marathon homepage

---
+++ public/src/slideshow.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    $("#slides > div:gt(0)").hide();
+
+	setInterval(function() { 
+	  $('#slides > div:first')
+	    .fadeOut(1000)
+	    .next()
+	    .fadeIn(1000)
+	    .end()
+	    .appendTo('#slides');
+	},5000);
+});
